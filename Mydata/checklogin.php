5d30b5988a407647234ef6b9e3fdc9b47b96bddf
MeanSoNat <nattaphon_wo@kkumail.com>
2022-01-18 17:25:48 +0700

    Login/Register

---
+++ Mydata/checklogin.php
@@ -0,0 +1,27 @@
+<?php
+    //$getID = $_POST['getid'];
+    isset( $_POST['getid'] ) ? $getID = $_POST['getid'] : $getID = "";
+    $getPass = $_POST['getpassword'];
+    //isset( $_POST['getPass'] ) ? $getID = $_POST['getpass'] : $getPass = "";
+
+    $password = md5($getPass);
+    //echo $password;
+    $link = mysqli_connect("localhost", "root", "", "datastudent");
+    if (!$link) {
+        exit('cannot connect to database.');
+    }
+
+    $sql = "SELECT * FROM information_student Where password_id='$password' and STID='$getID'";
+    $res = mysqli_query($link, $sql);
+    if (!$data = mysqli_fetch_array($res)){
+        echo "cannot password/id incorrect";
+        echo "<a href=login.html>back to login</a>";    
+    } else {
+        echo "Login successfull!" . "<br>";
+        echo "<tr>";
+        echo "<td>Hello {$data['FName_student']}</td>";
+        echo "</tr>";
+        echo "<a href = list.php>to list    </a>";
+    }
+    mysqli_close($link);
+?>
